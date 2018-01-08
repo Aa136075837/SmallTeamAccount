808e528df4305fcc5cc5a09acd5f14f70076c665
lizhiming <liwiseap@163.com>
2018-01-08 17:53:56 +0800

    1.添加 饼状图

--- app/src/main/java/com/smallteam/smallteamaccount/ui/fragment/AddAccountFragment.java
+++ app/src/main/java/com/smallteam/smallteamaccount/ui/fragment/AddAccountFragment.java
@@ -1,5 +1,9 @@
 package com.smallteam.smallteamaccount.ui.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -9,8 +13,11 @@ import com.smallteam.smallteamaccount.bean.NormalBean;
 import com.smallteam.smallteamaccount.http.HttpObserver;
 import com.smallteam.smallteamaccount.http.Load;
 import com.smallteam.smallteamaccount.presenter.AddAccountContract;
+import com.smallteam.smallteamaccount.utils.StringUtil;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.FormBody;
@@ -25,22 +32,36 @@ public class AddAccountFragment extends MVPBaseFragment<AddAccountContract.AddAc
     EditText mTestEt;
     @BindView(R.id.send_code)
     Button mSendCode;
+    @BindView(R.id.test_phone)
+    EditText mTestPhone;
+    @BindView(R.id.check_code)
+    Button mCheckCode;
+    private String mPhone;
 
     @Override
     protected void initEvents() {
-        mPresenter.sendCode("86", "17688163211");
 
         mSendCode.setOnClickListener(view -> {
-            RequestBody body = new FormBody.Builder()
-                    .add("phoneNum", "17688163211")
-                    .add("code", mTestEt.getText().toString().trim()).build();
-            HttpObserver<NormalBean> httpObserver = Load.createApi().niaBieLV(body).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
-                    .subscribeWith(new HttpObserver<NormalBean>(getActivity()) {
-                        @Override
-                        protected void call(NormalBean value) {
+            mPhone = mTestPhone.getText().toString().trim();
+            if (StringUtil.strIsPhone(getActivity(), mPhone)) {
+                mPresenter.sendCode("86", mPhone);
+            }
+        });
 
-                        }
-                    });
+        mCheckCode.setOnClickListener(view -> {
+            if(!StringUtil.strIsPhone(getActivity(), mPhone)){
+                return;
+            }
+            RequestBody body = new FormBody.Builder()
+                .add("phoneNum", mPhone)
+                .add("code", mTestEt.getText().toString().trim()).build();
+            HttpObserver<NormalBean> httpObserver = Load.createApi().niaBieLV(body).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new HttpObserver<NormalBean>(getActivity()) {
+                    @Override
+                    protected void call(NormalBean value) {
+
+                    }
+                });
             addObservable(httpObserver);
         });
     }
